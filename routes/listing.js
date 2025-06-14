b21edb71f560ab40efa685c808061dcbd0873177
Gopal1909 <iitmgopal1@gamil.com>
2025-06-14 14:37:34 +0530

    Add Project Files

---
+++ routes/listing.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
+
+//New Route
+router.get("/new", isLoggedIn, listingController.renderNewForm);
+
+//search
+router.get("/search", async(req,res) => {
+  const {q} = req.query;
+  const listings = await Listing.find({
+    $or: [
+      {title: new RegExp(q,'i')},
+      {description: new RegExp(q,'i')},
+      {location: new RegExp(q,'i')},
+      {country: new RegExp(q,'i')},
+    ]
+  });
+  res.render("listings/searchResults", {listings, q});
+})
+
+
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+
+//Edit Route
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.editListing)
+);
+
+module.exports = router;
